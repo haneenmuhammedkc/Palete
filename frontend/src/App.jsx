87cf6704b02dd57b4966b4fd1961f85e0b5e7c35
haneenmuhammedkc <haneenmuhammed369@gmail.com>
2026-05-11 11:16:16 +0530

    Updated UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MenuItems from './AdminPages/MenuItems'
 import AddMenuItems from './AdminPages/AddMenuItems'
+import AdminAnalytics from './AdminPages/AdminAnalytics'
+import AdminDashboard from './AdminPages/AdminDashboard'
+import AdminEmployees from './AdminPages/AdminEmployees'
+import AdminOrders from './AdminPages/AdminOrders'
+import AdminSettings from './AdminPages/AdminSettings'
+import LandingPage from './UserPages/LandingPage'
 
 const App = () => {
   return (
@@ -10,6 +16,13 @@ const App = () => {
         <Routes>
           <Route path='admin/menuitems' element={<MenuItems />} />
           <Route path='admin/addmenuitems' element={<AddMenuItems />} />
+          <Route path='admin/analytics' element={<AdminAnalytics />} />
+          <Route path='admin/dashboard' element={<AdminDashboard />} />
+          <Route path='admin/employees' element={<AdminEmployees />} />
+          <Route path='admin/orders' element={<AdminOrders />} />
+          <Route path='admin/settings' element={<AdminSettings />} />
+
+          <Route path='user/landing' element={<LandingPage />} />
         </Routes>
       </BrowserRouter>
     </div>
